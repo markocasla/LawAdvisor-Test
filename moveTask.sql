3960d49f244c3bbb761be995783cd6d73e3e43c7
markocasla <82507499+markocasla@users.noreply.github.com>
2021-04-15 19:50:47 +0800

    Add files via upload

---
+++ moveTask.sql
@@ -0,0 +1,26 @@
+DELIMITER $$
+
+USE `projectmanagementdb`$$
+
+DROP PROCEDURE IF EXISTS `moveTasks`$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `moveTasks`(
+	IN TASK_ORDER INT,
+	IN moveTo INT,
+	IN dead_line VARCHAR(200)    
+    )
+BEGIN
+	/*set current postion from rowstamp*/
+	SELECT @currentPosition := taskorder FROM tbltask WHERE rowstamp = TASK_ORDER;
+	/*MOVING OF TASKORDER IN BETWEEN OF CURRENT POSTION TO DESTIONATION*/	
+	IF (@currentPosition > moveTo) THEN
+		UPDATE tbltask SET taskorder = taskorder + 1 WHERE taskorder BETWEEN moveTo AND @currentPosition;
+	ELSE
+		UPDATE tbltask SET taskorder = taskorder - 1 WHERE taskorder BETWEEN @currentPosition AND moveTo;
+	END IF;
+	/*FOR THE MOVE OF EXACT TASK*/
+	UPDATE tbltask SET taskorder = moveTo, movecount = movecount + 1, deadline = dead_line WHERE rowstamp = TASK_ORDER;
+ 
+    END$$
+
+DELIMITER ;
